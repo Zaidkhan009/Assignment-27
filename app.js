5763c80abf17a58cd3b9d24257020eb932610fdc
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-21 02:25:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var allien_colour = "green";
+if (allien_colour == "green") {
+    console.log("The player just earned 5 point");
+}
+else if (allien_colour == "yellow") {
+    console.log("The player just earned 10 point");
+}
+else if (allien_colour == "red") {
+    console.log("The player just earned 15 point");
+}
+else {
+    console.log("Pleas select right colour");
+}
+//Version 2
+allien_colour = "red";
+if (allien_colour == "green") {
+    console.log("The player just earned 5 point");
+}
+else if (allien_colour == "yellow") {
+    console.log("The player just earned 10 point");
+}
+else if (allien_colour == "red") {
+    console.log("The player just earned 15 point");
+}
+else {
+    console.log("Pleas select right colour");
+}
+// version 3
+allien_colour = "yellow";
+if (allien_colour == "green") {
+    console.log("The player just earned 5 point");
+}
+else if (allien_colour == "yellow") {
+    console.log("The player just earned 10 point");
+}
+else if (allien_colour == "red") {
+    console.log("The player just earned 15 point");
+}
+else {
+    console.log("Pleas select right colour");
+}
---
+++ app.ts
@@ -0,0 +1,51 @@
+// let allien_colour: string ="green";
+
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point");
+// }
+
+// else if(allien_colour  == "yellow"){
+//     console.log("The player just earned 10 point");
+// }
+
+// else if(allien_colour  == "red"){
+//     console.log("The player just earned 15 point");
+// }
+// else{
+//     console.log("Pleas select right colour")
+// }
+
+
+// //Version 2
+// allien_colour ="red";
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point");
+// }
+
+// else if(allien_colour  == "yellow"){
+//     console.log("The player just earned 10 point");
+// }
+
+// else if(allien_colour  == "red"){
+//     console.log("The player just earned 15 point");
+// }
+// else{
+//     console.log("Pleas select right colour")
+// }
+
+// // version 3
+// allien_colour ="yellow";
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point");
+// }
+
+// else if(allien_colour  == "yellow"){
+//     console.log("The player just earned 10 point");
+// }
+
+// else if(allien_colour  == "red"){
+//     console.log("The player just earned 15 point");
+// }
+// else{
+//     console.log("Pleas select right colour")
+// }
